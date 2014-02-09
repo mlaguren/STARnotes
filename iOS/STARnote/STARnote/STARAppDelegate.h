0b2ef4fb8bcc1038b0255b724de9a406e3891b95
Melvin Laguren <melvin@laguren.net>
2014-02-08 23:22:03 -0800

    Initial iOS app

---
+++ iOS/STARnote/STARnote/STARAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STARAppDelegate.h
+//  STARnote
+//
+//  Created by Melvin Laguren on 2/8/14.
+//  Copyright (c) 2014 Melvin Laguren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STARAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/STARnote/STARnote/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  STARnote
+//
+//  Created by Melvin Laguren on 2/8/14.
+//  Copyright (c) 2014 Melvin Laguren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STARAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STARAppDelegate class]));
+    }
+}
